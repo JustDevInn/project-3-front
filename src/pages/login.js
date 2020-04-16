e089446bbb375d3d6e5af90f6e45cf75ba5a01f4
Justin Peeters <Peeters.justin@yahoo.com>
2020-04-16 09:04:31 +0200

    frontendchanged

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import DefaultLayout from "../layout/Default";
+import Form from "../layout/Form";
+import {signup} from "../utils/Auth";
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components/dist';
 
 class login extends Component {
     constructor(props) {
@@ -12,7 +17,32 @@ class login extends Component {
     render() {
         return (
             <div>
-                <h1>This is one awesome Login page!</h1>
+                {/* <div className="field">
+                    <p className="control has-icons-left has-icons-right">
+                      <input className="input" type="text" placeholder="Username">
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-envelope"></i>
+                      </span>
+                      <span className="icon is-small is-right">
+                        <i className="fas fa-user"></i>
+                      </span>
+                    </p>
+                </div>
+                <div className="field">
+                    <p className="control has-icons-left">
+                      <input className="input" type="password" placeholder="Password">
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-lock"></i>
+                      </span>
+                    </p>
+                </div>
+                <div className="field">
+                    <p className="control">
+                      <button className="button is-success">
+                        Login
+                      </button>
+                    </p>
+                </div> */}
             </div>
         )
     }
